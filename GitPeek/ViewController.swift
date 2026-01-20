d28b3639e33698ccb6aa1d3dae91b3987b46f003
Naushad Ali Khan <naushadkhank426@gmail.com>
2026-01-20 23:13:46 +0530

    feat: initial project setup with SceneDelegate window configuration

---
+++ GitPeek/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitPeek
+//
+//  Created by Naushad Ali Khan on 13/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
