64f32d2dd2c2b7b87df9cdd9ba9e504c5696e085
Naushad Ali Khan <naushadkhank426@gmail.com>
2026-01-21 23:58:49 +0530

    feat: SearchVC and FavouritesVC basic setup added

---
+++ GitPeek/FavouritesListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavouritesListVC.swift
+//  GitPeek
+//
+//  Created by Naushad Ali Khan on 21/01/26.
+//
+
+import UIKit
+
+class FavouritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GitPeek/SearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVC.swift
+//  GitPeek
+//
+//  Created by Naushad Ali Khan on 21/01/26.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
